c49c178079fd0e1f175251435e702017b359160e
Mohamed <mohakatalonya10@gmail.com>
2024-07-15 18:40:59 +0200

    finished the basics, now time for a small project

--- 17_oop.php
+++ 17_oop.php
@@ -4,8 +4,42 @@ class User {
   public $name;
   public $email;
   public $password;
-  
+
+  public function __construct($name, $email, $password) {
+    $this->name = $name;
+    $this->email = $email;
+    $this->password = $password;
+  }
+
+
+  function display_name(){
+    echo $this->name;
+  }
+
 }
+
+
+Class Employee extends User {
+  private $title;
+
+  public function __construct($name, $email, $password, $title) {
+  parent::__construct($name, $email, $password);
+  $this->title = $title;
+}
+
+ function get_title(){
+  return $this->title;
+}
+}
+
+// $user1 = new User('MOH', 'email', 'password');
+// $user1->display_name();
+
+$employee1 = new Employee('moh', 'mail', 'password', 'manager');
+echo $employee1->get_title();
+
+
+// var_dump($user1);
 
 
 ?>
